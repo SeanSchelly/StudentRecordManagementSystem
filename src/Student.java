cdd30016aa2d295a1cbea0358c23613a81d4d087
seanschellhas <seanschellhas7@gmail.com>
2026-06-13 11:38:29 +0300

    Basic Code Structure established; student class, constructor, fields, main method, importing classes (from io and util)

---
+++ src/Student.java
@@ -0,0 +1,13 @@
+public class Student {
+    private int ID;
+    private String name;
+    private String dept;
+    private int gpa;
+
+    public Student(int ID, String name, String dept, int gpa) {
+        this.ID = ID;
+        this.name = name;
+        this.dept = dept;
+        this.gpa = gpa;
+    }
+}
