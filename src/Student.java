984ad4883c25cf60034c9464a487355945597675
SeanSchelly <seanschellhas7@gmail.com>
2026-06-14 00:13:00 +0300

    Minor fix

--- src/Student.java
+++ src/Student.java
@@ -2,7 +2,7 @@ public class Student {
     private int ID;
     private String name;
     private String dept;
-    private int gpa;
+    private double gpa;
     //Constructor
     public Student(int ID, String name, String dept, double gpa) {
         this.ID = ID;
