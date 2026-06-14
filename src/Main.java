48f62c5d7723fb54dbb8c6a3a5bc5421f4d1e9ea
SeanSchelly <seanschellhas7@gmail.com>
2026-06-14 19:19:19 +0300

    Major overhaul: Fixed decision-making, closed printwriter objects, fixed Name not taking input for new student(escape sequence hogging buffer), automation of student registration, students array for object iteration and storage

--- src/Main.java
+++ src/Main.java
@@ -10,179 +10,146 @@ public class Main {
         Student s2 = new Student (0, "name", "dept", 0.0);
         Student s3 = new Student (0, "name", "dept", 0.0);
         Student s4 = new Student (0, "name", "dept", 0.0);
+        //Global students array
+        Student[] studentsArray = {s1, s2, s3, s4};
         //
         System.out.println("Objects instantiated.");
         System.out.println("Checking for Student folder...");
         File studentsFolder = new File("Students Folder");
+        //global scanner
+        Scanner input = new Scanner(System.in);
         if (!(studentsFolder.exists())) {
-            Scanner input1 = new Scanner(System.in);
+
             System.out.println("No folder detected, shall we add one for you?(yes/no)");
-            String ans1 = input1.nextLine();
+            String ans1 = input.nextLine();
             if(ans1.equals("yes")) {
                 System.out.println("Creating student record folder...");
                 studentsFolder.mkdir();
-                System.out.println("Folder created. Restart program to proceed with record creation or file details display.");
+                System.out.println("Folder created. Restart program to proceed with record creation.");
             } else {
                 System.out.println("Can't continue without folder to store data, program finished.");
             }
         } else {
-            System.out.println("Folder detected. Would you like to know file data(1) or continue(2) with record creation? (1/2)");
-            Scanner input2 = new Scanner(System.in);
-            int ans = input2.nextInt();
-            switch(ans) {
-                case 1:
-                    System.out.println("Folder details: ");
-                    System.out.println("-------------------------------");
-                    System.out.println("Name: "+studentsFolder.getName());
-                    System.out.println("Path: "+studentsFolder.getPath());
-                    System.out.println("Size: "+studentsFolder.length());
-                    //TODO: Make last modified in date/time format
-                    System.out.println("Last Modified(Date): "+studentsFolder.lastModified());
+            System.out.println("Folder detected. Would you like to continue with record creation? (yes/no)");
+            String ans2 = input.nextLine();
+            switch(ans2) {
+                case "no":
+                    System.out.println("Exiting...");
                     break;
-                case 2:
+                case "yes":
                     System.out.println("Record creation: ");
                     System.out.println("-------------------------------");
-                    System.out.println("Initializing fileOut object...");
-                        try(PrintWriter fileOut = new PrintWriter("Students Folder\\StudentsFile.txt")) {
-                            System.out.println("Done. Proceeding...");
-                            System.out.println("-------------------------------");
-                            //
-                            System.out.println("Student 1: ");
-                            s1.setID(1);
-                            System.out.println("ID:");
-                            fileOut.print(s1.getID()+" ");
-                            System.out.println("ID set.(preset)");
-                            //
-                            //
-                            System.out.println("Name: ");
-                            Scanner input_1 = new Scanner(System.in);
-                            String ans1 = input_1.nextLine();
-                            s1.setName(ans1);
-                            fileOut.print(s1.getName()+" ");
-                            System.out.println("Name set.");
-                            //
-                            //
-                            System.out.println("Department: ");
-                            Scanner input_2 = new Scanner(System.in);
-                            String ans2 = input_2.nextLine();
-                            s1.setName(ans2);
-                            fileOut.print(s1.getDept()+" ");
-                            System.out.println("Department set.");
-                            //
-                            //
-                            System.out.println("GPA: ");
-                            Scanner input_3 = new Scanner(System.in);
-                            double ans3 = input_1.nextDouble();
-                            s1.setGpa(ans3);
-                            fileOut.print(s1.getGpa());
-                            System.out.println("GPA set.");
-                            fileOut.println();
-                            System.out.println("-------------------------------");
-                            //
-                            //Student 2
-                            System.out.println("Student 2: ");
-                            s2.setID(2);
-                            System.out.println("ID:");
-                            fileOut.print(s2.getID()+" ");
-                            System.out.println("ID set.(preset)");
-                            //
-                            //
-                            System.out.println("Name: ");
-                            Scanner input_12 = new Scanner(System.in);
-                            String ans12 = input_12.nextLine();
-                            s2.setName(ans12);
-                            fileOut.print(s2.getName()+" ");
-                            System.out.println("Name set.");
-                            //
-                            //
-                            System.out.println("Department: ");
-                            Scanner input_21 = new Scanner(System.in);
-                            String ans21 = input_21.nextLine();
-                            s2.setName(ans21);
-                            fileOut.print(s2.getDept()+" ");
-                            System.out.println("Department set.");
-                            //
-                            //
-                            System.out.println("GPA: ");
-                            Scanner input_31 = new Scanner(System.in);
-                            double ans31 = input_31.nextDouble();
-                            s2.setGpa(ans31);
-                            fileOut.print(s2.getGpa());
-                            System.out.println("GPA set.");
-                            fileOut.println();
-                            System.out.println("-------------------------------");
-                            //
-                            //Student 3
-                            System.out.println("Student 3: ");
-                            s3.setID(3);
-                            System.out.println("ID:");
-                            fileOut.print(s3.getID()+" ");
-                            System.out.println("ID set.(preset)");
-                            //
-                            //
-                            System.out.println("Name: ");
-                            Scanner input_13 = new Scanner(System.in);
-                            String ans13 = input_13.nextLine();
-                            s3.setName(ans13);
-                            fileOut.print(s3.getName()+" ");
-                            System.out.println("Name set.");
-                            //
-                            //
-                            System.out.println("Department: ");
-                            Scanner input_22 = new Scanner(System.in);
-                            String ans22 = input_22.nextLine();
-                            s3.setName(ans22);
-                            fileOut.print(s3.getDept()+" ");
-                            System.out.println("Department set.");
-                            //
-                            System.out.println("GPA: ");
-                            Scanner input_32 = new Scanner(System.in);
-                            double ans32 = input_32.nextDouble();
-                            s2.setGpa(ans32);
-                            fileOut.print(s2.getGpa());
-                            System.out.println("GPA set.");
-                            fileOut.println();
-                            System.out.println("-------------------------------");
-                            //
-                            //Student 4
-                            System.out.println("Student 4: ");
-                            s4.setID(4);
-                            System.out.println("ID:");
-                            fileOut.print(s4.getID()+" ");
-                            System.out.println("ID set.(preset)");
-                            //
-                            //
-                            System.out.println("Name: ");
-                            Scanner input_14 = new Scanner(System.in);
-                            String ans14 = input_14.nextLine();
-                            s4.setName(ans14);
-                            fileOut.print(s4.getName()+" ");
-                            System.out.println("Name set.");
-                            //
-                            //
-                            System.out.println("Department: ");
-                            Scanner input_23 = new Scanner(System.in);
-                            String ans23 = input_23.nextLine();
-                            s4.setName(ans23);
-                            fileOut.print(s4.getDept()+" ");
-                            System.out.println("Department set.");
-                            //
-                            //
-                            System.out.println("GPA: ");
-                            Scanner input_33 = new Scanner(System.in);
-                            double ans33 = input_33.nextDouble();
-                            s4.setGpa(ans33);
-                            fileOut.print(s4.getGpa());
-                            System.out.println("GPA set.");
-                            fileOut.println();
-                            System.out.println("-------------------------------");
-                            //
+                    System.out.println("Creating files...");
+                    try(PrintWriter fileOut1 = new PrintWriter("Students Folder\\StudentsFile1.txt");
+                        PrintWriter fileOut2 = new PrintWriter("Students Folder\\StudentsFile2.txt");
+                        PrintWriter fileOut3 = new PrintWriter("Students Folder\\StudentsFile3.txt");
+                        PrintWriter fileOut4 = new PrintWriter("Students Folder\\StudentsFile4.txt")) {
 
-                            break;
+                        PrintWriter[] files = {fileOut1, fileOut2, fileOut3, fileOut4};
+                        System.out.println("Done.");
+                        System.out.println("-------------------------------");
+
+                        //student inputs
+                        System.out.println("ID: ");
+                        for(int i=0; i<studentsArray.length; i++) {
+                            studentsArray[i].setID(i+1);
+                        }
+                        System.out.println("ID set(preset)");
+                        System.out.println("-------------------------------");
+                        //
+                        System.out.println("Name: ");
+                        for(int i=0; i<studentsArray.length; i++) {
+                            String name = input.nextLine();
+                            studentsArray[i].setName(name);
+                        }
+                        System.out.println("Name set.");
+                        System.out.println("-------------------------------");
+                        //
+                        System.out.println("Department: ");
+                        for(int i=0; i<studentsArray.length; i++) {
+                            String dept = input.nextLine();
+                            studentsArray[i].setDept(dept);
+                        }
+                        //
+                        System.out.println("Department set.");
+                        System.out.println("-------------------------------");
+                        System.out.println("GPA: ");
+                        for(int i=0; i<studentsArray.length; i++) {
+                            double gpa = input.nextDouble();
+                            studentsArray[i].setGpa(gpa);
+                        }
+                        //
+                        System.out.println("GPA set.");
+                        System.out.println("-------------------------------");
+                        System.out.println("Adding entries to files... ");
+                        System.out.println("-------------------------------");
+                        for(int i=0; i<studentsArray.length; i++) {
+                            files[i].println(studentsArray[i].getID()+" "+studentsArray[i].getName()+" "+studentsArray[i].getDept()+" "+studentsArray[i].getGpa());
+                        }
+                        //
+                        System.out.println("-------------------------------");
+                        System.out.println("Closing streams...");
+                        //
+                        fileOut1.close();
+                        fileOut2.close();
+                        fileOut3.close();
+                        fileOut4.close();
+                        //
+                        System.out.println("-------------------------------");
+                        System.out.println("Done.");
+                        System.out.println("-------------------------------");
+                        System.out.println("Your students are recorded successfully. What would you like to do now?");
+                        System.out.println("1. Add Student");
+                        System.out.println("2. Search Student By ID");
+                        System.out.println("3. Update Student Information");
+                        System.out.println("4. Delete Student");
+                        System.out.println("5. Display All Students");
+                        //
+                        int actionChoice = input.nextInt();
+                        switch(actionChoice) {
+                            case 1:
+                                System.out.println("-------------------------------");
+                                System.out.println("Initializing new student object...");
+                                System.out.println("-------------------------------");
+                                Student s5 = new Student (0, "name", "dept", 0.0);
+                                //New student array
+                                studentsArray = new Student[]{s1, s2, s3, s4, s5};
+                                //
+                                System.out.println("Done. Adding student to new file...");
+                                try(PrintWriter fileOut5 = new PrintWriter("Students Folder\\StudentsFile5.txt")) {
+                                    System.out.println("ID: ");
+                                    studentsArray[4].setID(5);
+                                    System.out.println("ID set. (preset)");
+                                    //
+                                    String nameNew = input.nextLine(); // consumes escape sequence that is block buffer memory to allow name input
+                                    System.out.println("Name: ");
+                                    nameNew = input.nextLine();
+                                    s5.setName(nameNew);
+                                    System.out.println("Name set.");
+                                    //
+                                    System.out.println("Department: ");
+                                    String deptNew = input.nextLine();
+                                    studentsArray[4].setDept(deptNew);
+                                    System.out.println("Department set.");
+                                    //
+                                    System.out.println("GPA: ");
+                                    double gpaNew = input.nextDouble();
+                                    studentsArray[4].setGpa(gpaNew);
+                                    System.out.println("GPA set.");
+
+                                    fileOut5.println(studentsArray[4].getID()+" "+studentsArray[4].getName()+" "+studentsArray[4].getDept()+" "+studentsArray[4].getGpa());
+                                    System.out.println("Done.");
+                                } catch(IOException e){
+                                    e.printStackTrace();
+                                }
+                            case 2:
+
+                        }
+
                     } catch(IOException e) {
                         e.printStackTrace();
-                }
-
+                    }
+                    break;
                 default:
                     System.out.println("Err: Invalid Option. Rerun Program.");
 
